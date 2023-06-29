20b3b3ae8224072357df62627f448d730e971059
Sourabh Patil <81312909+SourabhGPatil@users.noreply.github.com>
2023-06-29 23:51:51 +0530

    Added comments to locator.dart

--- FlutterNewsApp/lib/services/locator.dart
+++ FlutterNewsApp/lib/services/locator.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/services.dart';
 import 'package:news_app_api/services/push_notification-service.dart';
 
+// Import the GetIt package for dependency injection
 GetIt locator = GetIt.instance;
+
+// Function to set up the dependency injection container
 void setupLocator() {
+  // Register the PushNotificationService as a lazy singleton in the locator
   locator.registerLazySingleton(() => PushNotificationService());
 }
