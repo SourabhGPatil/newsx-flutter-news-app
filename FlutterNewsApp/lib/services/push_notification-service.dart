91b8f07f9f4bc321697e84a374446586d88276c9
Sourabh Patil <81312909+SourabhGPatil@users.noreply.github.com>
2023-06-29 23:54:26 +0530

    Added comments to push_notification-service.dart

--- FlutterNewsApp/lib/services/push_notification-service.dart
+++ FlutterNewsApp/lib/services/push_notification-service.dart
@@ -5,23 +5,27 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 class PushNotificationService {
   final FirebaseMessaging _fcm = FirebaseMessaging();
 
+  // Function to initialize the push notification service
   Future initialise() async {
     if(Platform.isIOS) {
-      // request permissions if we're on Android
+       // Request permissions for push notifications if we're on iOS
       _fcm.requestNotificationPermissions(IosNotificationSettings());
     }
 
+    // Configure the Firebase Messaging instance
     _fcm.configure(
-      //Called when the app is in the foreground and we receive a push notification.
+      // Called when the app is in the foreground and we receive a push notification
       onMessage: (Map<String, dynamic> message) async {
         print('onMessage: $message');
       },
+      
       //Called when the app has been closed completely
       //and it's opened from the push notification directly.
       onLaunch: (Map<String, dynamic> message) async {
         print('onMessage: $message');
       },
-      //Called whenb the app is in the background
+      
+      //Called when the app is in the background
       //and it's opened from the push notification.
       onResume: (Map<String, dynamic> message) async {
         print('onMessage: $message');
