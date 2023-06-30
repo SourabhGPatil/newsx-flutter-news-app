857dea3a17dc2ce6274df5c0642b4111869c3b88
Sourabh Patil <81312909+SourabhGPatil@users.noreply.github.com>
2023-06-30 18:43:12 +0530

    Added comments to news.dart

--- FlutterNewsApp/lib/helper/news.dart
+++ FlutterNewsApp/lib/helper/news.dart
@@ -4,22 +4,22 @@ import 'dart:convert';
 
 import 'package:news_app_api/secret.dart';
 
+// Class for fetching news
 class News {
+  List<Article> news = [];
 
-  List<Article> news  = [];
-
-  Future<void> getNews() async{
-
+  // Function to get top headlines news
+  Future<void> getNews() async {
     String url = "http://newsapi.org/v2/top-headlines?country=in&excludeDomains=stackoverflow.com&sortBy=publishedAt&language=en&apiKey=${apiKey}";
 
     var response = await http.get(url);
 
     var jsonData = jsonDecode(response.body);
 
-    if(jsonData['status'] == "ok"){
-      jsonData["articles"].forEach((element){
-
-        if(element['urlToImage'] != null && element['description'] != null){
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          // Create an Article object and add it to the news list
           Article article = Article(
             title: element['title'],
             author: element['author'],
@@ -31,34 +31,27 @@ class News {
           );
           news.add(article);
         }
-
       });
     }
-
-
   }
-
-
 }
 
-
+// Class for fetching news for a specific category
 class NewsForCategorie {
+  List<Article> news = [];
 
-  List<Article> news  = [];
-
-  Future<void> getNewsForCategory(String category) async{
-
-    /*String url = "http://newsapi.org/v2/everything?q=$category&apiKey=${apiKey}";*/
+  // Function to get news for a specific category
+  Future<void> getNewsForCategory(String category) async {
     String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=${apiKey}";
 
     var response = await http.get(url);
 
     var jsonData = jsonDecode(response.body);
 
-    if(jsonData['status'] == "ok"){
-      jsonData["articles"].forEach((element){
-
-        if(element['urlToImage'] != null && element['description'] != null){
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          // Create an Article object and add it to the news list
           Article article = Article(
             title: element['title'],
             author: element['author'],
@@ -70,14 +63,7 @@ class NewsForCategorie {
           );
           news.add(article);
         }
-
       });
     }
-
-
   }
-
-
 }
-
-
