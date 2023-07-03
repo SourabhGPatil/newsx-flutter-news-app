33c12106925294b884c87abf91dc7562cb69ca73
Sourabh Patil <81312909+SourabhGPatil@users.noreply.github.com>
2023-07-03 19:54:44 +0530

    Cleaned splash_screen.dart

--- FlutterNewsApp/lib/views/splash_screen.dart
+++ FlutterNewsApp/lib/views/splash_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart';
 import 'homepage.dart';
 
 class SplashScreen extends StatefulWidget{
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -13,17 +12,19 @@ class SplashScreen extends StatefulWidget{
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-
     super.initState();
+
+    // Set a timer to navigate to the HomePage after 5 seconds
     Timer(Duration(seconds: 5),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                    HomePage()
-            )
+        ()=>Navigator.pushReplacement(context,
+        MaterialPageRoute(builder:
+            (context) =>
+                HomePage()
+        )
         )
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +35,6 @@ class _SplashScreenState extends State<SplashScreen> {
           gradient: LinearGradient(
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
-
               colors: [Color(0xFFFF800B),Color(0xFFCE1010),]
           ),
         ),
@@ -49,7 +49,9 @@ class _SplashScreenState extends State<SplashScreen> {
                   height: 300.0,
                   width: 300.0,
                 ),
-                Text("NEWS MOBILE APPLICATION\nUSING THE NEWS API",textAlign:TextAlign.center,
+                Text(
+                  "NEWS MOBILE APPLICATION\nUSING THE NEWS API",
+                  textAlign:TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
@@ -57,7 +59,9 @@ class _SplashScreenState extends State<SplashScreen> {
                   ),
                 ),
 
-                Text("\n\nDeveloped with Flutter and Dart by\n Shreyas Deshpande - 2JR18CS072\n Sourabh Patil - 2JR18CS079\n Veer Jadimath - 2JR18CS091\n Vikyath Kamat - 2JR18CS093",textAlign:TextAlign.center,
+                Text(
+                  "\n\nDeveloped with Flutter and Dart by\n Shreyas Deshpande - 2JR18CS072\n Sourabh Patil - 2JR18CS079\n Veer Jadimath - 2JR18CS091\n Vikyath Kamat - 2JR18CS093",
+                  textAlign:TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.normal,
@@ -65,7 +69,9 @@ class _SplashScreenState extends State<SplashScreen> {
                   ),
                 ),
 
-                Text("\n\n\n\n\n\n\n\n © Computer Science and Engineering Department,\nJain College of Engineering and Research, Udyambag, Belgaum, Karnataka",textAlign:TextAlign.center,
+                Text(
+                  "\n\n\n\n\n\n\n\n © Computer Science and Engineering Department,\nJain College of Engineering and Research, Udyambag, Belgaum, Karnataka",
+                  textAlign:TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
